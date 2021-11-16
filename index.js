d87eaed4983e5209b9a48c4f7bf2d9206a3db987
PooiaFerdowsi <pooia.ferdowsi.is.developer@gmail.com>
2021-11-16 10:03:32 +0330

    You may need more readability.
    Blocks are important. they help readers to understand or misunderstand.

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ const test = (name , callback)=>{
 }
 const expect = (a)=>{
     return {
-        toBe: (b)=>{
-        if (typeof a === "object" && a !== null){
-            return JSON.stringify(a) === JSON.stringify(b)
-        }else{
-            return a === b
+        toBe: (b)=> {
+            if (typeof a === "object" && a !== null){
+                return JSON.stringify(a) === JSON.stringify(b)
+            }else{
+                return a === b
+            }
         }
     }
-}
 }
 module.exports = {test,expect}
