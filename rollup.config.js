78904e22a536b08718dbf5b8aa5a0f7316883851
mehan <mehan.alavi.88.majd@gmail.com>
2021-11-17 20:44:12 +0330

    correcting the path in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { terser } from "rollup-plugin-terser"
 export default{
-    input: 'src/index.js',
+    input: 'packages/index.js',
     // outputs for iife and cjs with minified code
     output: [
         {
