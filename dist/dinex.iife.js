d1dc16d141e1152d0f331902016f7f3b0d7b7b38
mehan <mehan.alavi.88.majd@gmail.com>
2021-11-23 22:10:00 +0330

    deleted dist/ from .gitignore

---
+++ dist/dinex.iife.js
@@ -0,0 +1,32 @@
+var index = (function (exports) {
+    'use strict';
+
+    const expect = (a)=>{
+        return {
+            toBe: (b)=>{
+            if (typeof a === "object" && a !== null){
+                return JSON.stringify(a) === JSON.stringify(b)
+            }else {
+                return a === b
+            }
+        }
+    }
+    };
+
+    const test = (name , callback)=>{
+        let passed = "failed";
+        if (callback()===true){
+            passed = "passed";
+        }
+        console.log(`${name} test : ${passed}`);
+        return callback()===true;
+    };
+
+    exports.expect = expect;
+    exports.test = test;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+    return exports;
+
+})({});
---
+++ dist/dinex.js
@@ -0,0 +1,27 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const expect = (a)=>{
+    return {
+        toBe: (b)=>{
+        if (typeof a === "object" && a !== null){
+            return JSON.stringify(a) === JSON.stringify(b)
+        }else {
+            return a === b
+        }
+    }
+}
+};
+
+const test = (name , callback)=>{
+    let passed = "failed";
+    if (callback()===true){
+        passed = "passed";
+    }
+    console.log(`${name} test : ${passed}`);
+    return callback()===true;
+};
+
+exports.expect = expect;
+exports.test = test;
