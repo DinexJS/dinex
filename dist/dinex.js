760fee9be87c197878fabab92c10d484a6d400cf
mehan <74927578+mehanalavimajd@users.noreply.github.com>
2021-12-03 13:16:16 +0330

    Update dinex.js

--- dist/dinex.js
+++ dist/dinex.js
@@ -12,7 +12,6 @@ const expect = (a) => {
       }
     },
     Not: {
-      return {
         toBe: (b) => {
        if (typeof a === 'object' && a !== null) {
         return JSON.stringify(a) !== JSON.stringify(b);
